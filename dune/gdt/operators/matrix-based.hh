8b63661e36575e99edf6f0319fce7be733705123
Felix Schindler <felix.schindler@wwu.de>
2018-08-31 00:46:26 +0200

    improve assembly detection

--- dune/gdt/operators/matrix-based.hh
+++ dune/gdt/operators/matrix-based.hh
@@ -316,7 +316,8 @@ public:
     , assembled_(false)
   {
     // to detect assembly
-    this->append([&](const auto&) { assembled_ = true; });
+    this->append(
+        [](/*prepare nothing*/) {}, [](const auto&) { /*apply nothing*/ }, [&](/*finalize*/) { assembled_ = true; });
   }
 
   /**
